319caea0d953b4007cb9485b6a21ceefcdac22e0
Komaedamaker <daniel.dellanos@colorado.edu>
2022-02-28 03:59:09 +0000

    completed rec 6

---
+++ QueueLL.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include "QueueLL.hpp"
+
+using namespace std;
+
+QueueLL::QueueLL()
+{
+    queueFront = nullptr;
+    queueEnd = nullptr;
+}
+
+QueueLL::~QueueLL()
+{
+    while( !isEmpty() )
+        dequeue();
+}
+
+bool QueueLL::isEmpty()
+{
+    /*if(queueFront == nullptr || queueEnd == nullptr)
+        return true;
+    return false;*/
+    return (!queueFront || !queueEnd);
+}
+
+// TODO
+void QueueLL::enqueue(int key)
+{
+    Node *nn = new Node;
+    nn->key = key;
+    nn->next = nullptr;
+
+    if (!isEmpty()) {
+        queueEnd->next = nn;
+        queueEnd = nn;
+    } else {
+        queueEnd = nn;
+        queueFront = nn;
+    }
+
+}
+
+//TODO
+void QueueLL::dequeue()
+{
+    if(!isEmpty())
+    {
+        Node* temp = queueFront;
+        queueFront = queueFront->next;
+        delete temp;
+    }
+    else{
+        cout<<"queue is empty. can not deque"<<endl;
+    }
+}
+
+int QueueLL::peek()
+{
+    if( !isEmpty() )
+        return queueFront->key;
+    else
+    {
+        cout<< " queue is empty. can not peek"<<endl;
+        return -1;
+    }
+    //return 0;
+}
